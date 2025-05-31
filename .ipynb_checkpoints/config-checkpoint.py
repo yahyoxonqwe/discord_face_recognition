2ba9f1c4fb394640a353aa092541479a25082be5
yahyoxonqwe <yahyoxonrahimov@gmail.com>
2025-05-31 16:16:39 +0500

    commit

--- .ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,7 +0,0 @@
-
-DISCORD_TOKEN = ''
-MODELS_PATH = 'C:/Users/rog/AERO/face_recognition/xb_face_recognition/models/buffalo_s'
-DATABASE_FOLDER = 'C:/Users/rog/AERO/face_recognition/xb_face_recognition/dataset'
-DATABASE_PATH = 'C:/Users/rog/AERO/face_recognition/xb_face_recognition/xb_database.db'
-images_path =  'C:/Users/rog/AERO/face_recognition/xb_face_recognition/images'
-EMPLOYEES_JSON = 'C:/Users/rog/AERO/face_recognition/xb_face_recognition/employees.json'
